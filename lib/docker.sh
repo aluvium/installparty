0497b8cd5e802dd7304960e164b8cf57406a9c66
aluvium <aluvium@protonmail.com>
2022-06-09 07:53:33 +0000

    docker installation

--- lib/docker.sh
+++ lib/docker.sh
@@ -2,10 +2,21 @@
 docker() {
 	echo ""
 	echo "[*] Updating system.."
+	echo ""
 	apt-get update -y
-	echo  [*] Checking if docker exist..
-	[[ docker --version ]] 2> /dev/null && echo "Docker already installed." && exit 1 
+	echo "[*] Checking if docker exist.."
+	echo ""
+	[[ sh -c "docker --version" ]] 2> /dev/null || echo "Docker already installed." && exit 1 
 	echo "[*] Adding docker public-key and updating source list.."
+	echo ""
 	wget -O- https://download.docker.com/linux/debian/gpg | apt-key add -
-	sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu/ $(lsb_release -cs) stable" >> /etc/apt/sources.list'
+	
+	sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu/ $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list'
+        echo "[*] Installing Docker.. " 
+	echo ""
+	apt-get update -y
+	apt-get install docker-ce -y
+	[[ sh -c "docker --version" ]] || echo "[*] Ups.. something went wrong." && exit 2
+	echo "[*] Instalation completed."
+	echo ""
 }
