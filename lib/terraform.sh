bfd94eb1b9eb4e5c7e6d2de57d7d16ee9d082c06
aluvium <aluvium@protonmail.com>
2022-06-10 14:56:28 +0000

    terraform installer

--- lib/terraform.sh
+++ lib/terraform.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+var1="terraform"
 terraform() {
-	echo "hello word"
+	echo ""
+	echo "[*] Updating system.."
+	echo ""
+	apt-get update -y
+	echo "[*] Checking if $var1 exist.."
+	echo ""
+	if [ -f /usr/bin/$var1 ] ; then echo "$var1 already installed." && exit 0 
+	fi
+	echo "[*] Adding $var1 public-key and updating source list.."
+	echo ""
+	curl -L https://apt.releases.hashicorp.com/gpg | apt-key add -
+	
+	sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main" > /etc/apt/sources.list.d/terraform.list'
+        echo "[*] Installing $var1.. " 
+	echo ""
+	apt-get update -y
+	apt-get install $var1
+	echo "[*] Instalation completed."
+	echo ""
 }
