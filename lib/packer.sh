d5d84de27fc6222d151daf9126bb76d21f16bd9d
aluvium <aluvium@protonmail.com>
2022-06-19 16:02:46 +0000

    hashicorp install

--- lib/packer.sh
+++ lib/packer.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 var="packer"
-packer() {
-	echo ""
-	echo "[*] Updating system.."
-	echo ""
+packer() 
+        { 
 	apt-get update -y
-	echo "[*] Checking if $var exist.."
-	echo ""
 	if [ -f /usr/bin/$var ] ; then echo "$var already installed." ; exit 0 
 	fi
 	echo "[*] Adding $var public-key and updating source list.."
 	echo ""
 	curl -L https://apt.releases.hashicorp.com/gpg | apt-key add -
 	
-	sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main" > /etc/apt/sources.list.d/packer.list'
-        echo "[*] Installing $var.. " 
-	echo ""
+	if [ -f /etc/apt/sources.list.d./hashicorp.list ] ; then sudo rm '/etc/apt/sources.list.d/hashicorp.list' ; fi
+
+	sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main" > /etc/apt/sources.list.d/hashicorp.list'
 	apt-get update -y
 	apt-get install $var
 	echo "[*] Instalation completed."
--- lib/terraform.sh
+++ lib/terraform.sh
@@ -2,19 +2,11 @@
 var1="terraform"
 terraform() {
 	apt-get update -y
-	echo "[*] Checking if $var1 exist.."
-	echo ""
-	if [ -f /usr/bin/$var1 ] ; then echo "$var1 already installed." && exit 0 
-	fi
-	echo "[*] Adding $var1 public-key and updating source list.."
-	echo ""
+	if [ -f /usr/bin/$var1 ] ; then echo "$var1 already installed." && exit 0 ; fi
 	curl -L https://apt.releases.hashicorp.com/gpg | apt-key add -
 	
-	if ! [ -f /etc/apt/sources.list.d/hashicorp.list  ] ; then 
-	  sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main" > /etc/apt/sources.list.d/hashicorp.list'
-        fi	
-	echo "[*] Installing $var1.. " 
-	echo ""
+	if [ -f /etc/apt/sources.list.d/hashicorp.list  ] ; then sudo rm '/etc/apt/sources.list.d/hashicorp.list'; fi
+	sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main" > /etc/apt/sources.list.d/hashicorp.list'
 	apt-get update -y
         apt-get install $var1
 	echo "[*] Instalation completed."
