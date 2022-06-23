6f2f7d1ba190be1ee195649bd8d526787de1dbd1
aluvium <aluvium@protonmail.com>
2022-06-23 19:57:49 +0000

    Docker and docker-compose in one file

--- lib/docker-compose.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker_compose() {
-	echo "hello word"
-}
--- lib/docker.sh
+++ lib/docker.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 var2="docker"
 docker() {
-	echo ""
-	echo "[*] Updating system.."
-	echo ""
 	apt-get update -y
 	echo "[*] Checking if $var2 exist.."
 	echo ""
-	if [ -f /usr/bin/$var2 ] ; then echo "$var2 already installed." && exit 0
-	fi	
+        if [ -f /usr/bin/$var1 ] ; then echo "$var1 already installed." && exit 0 ; fi  
+	
+	# KEY
 	echo "[*] Adding $var2 public-key and updating source list.."
 	echo ""
 	wget -O- https://download.docker.com/linux/debian/gpg | apt-key add -
 	
+        # SOURCE
+        if [ -f /etc/apt/sources.list.d/docker.list  ] ; then sudo rm '/etc/apt/sources.list.d/docker.list'; fi
 	sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu/ $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list'
         echo "[*] Installing $var2.. " 
 	echo ""
@@ -20,4 +20,11 @@ docker() {
 	apt-get install docker-ce -y
 	echo "[*] Instalation completed."
 	echo ""
+}
+docker_compose() {
+       if ! [ -f /usr/bin/docker ] ; then docker ; fi
+       
+       sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+       sudo chmod +x /usr/local/bin/docker-compose
+       docker-compose --version
 }
