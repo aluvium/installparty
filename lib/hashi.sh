667010fa3ccd8f669ebcb55d5ae5f552fb3a87f6
aluvium <aluvium@protonmail.com>
2022-06-25 18:39:25 +0000

    packer

--- lib/hashi.sh
+++ lib/hashi.sh
@@ -4,11 +4,25 @@ terraform() {
 	if [ -f /usr/bin/$var1 ] ; then echo "$var1 already installed." && exit 0 ; fi
 	apt-get update -y
 	curl -L https://apt.releases.hashicorp.com/gpg | apt-key add -
-	
 	if [ -f /etc/apt/sources.list.d/hashicorp.list  ] ; then sudo rm '/etc/apt/sources.list.d/hashicorp.list'; fi
 	sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main" > /etc/apt/sources.list.d/hashicorp.list'
 	apt-get update -y
         apt-get install $var1
 	echo "[*] Instalation completed."
 	echo ""
+}
+packer() {
+	var2=$1
+        apt-get update -y
+        if [ -f /usr/bin/$var1 ] ; then echo "$var1 already installed." ; exit 0
+        fi
+        echo "[*] Adding $var1 public-key and updating source list.."
+        echo ""
+        curl -L https://apt.releases.hashicorp.com/gpg | apt-key add -
+        if [ -f /etc/apt/sources.list.d./hashicorp.list ] ; then sudo rm '/etc/apt/sources.list.d/hashicorp.list' ; fi
+        sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main" > /etc/apt/sources.list.d/hashicorp.list'
+        apt-get update -y
+        apt-get install $var1
+        echo "[*] Instalation completed."
+        echo ""
 }
