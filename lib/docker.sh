6854b37e0cce9a4c5c0d3f0c8ef3a9002717dc5e
aluvium <aluvium@protonmail.com>
2022-07-30 16:31:47 +0000

    jenkins installation

--- lib/docker.sh
+++ lib/docker.sh
@@ -1,19 +1,18 @@
 #!/bin/env bash
 docker() {
-        var1="docker"
-        echo "[*] Checking if $var1-compose exist.."
+        echo "[*] Checking if $1-compose exist.."
         echo ""
-	if [ -f /usr/bin/$var1 ] ; then echo "$var1 already installed." && exit 0 ; fi  
+	if [ -f /usr/bin/$1 ] ; then echo "$1 already installed." && exit 0 ; fi  
 	apt-get update -y
 	# KEY
-	echo "[*] Adding $var1 public-key and updating source list.."
+	echo "[*] Adding $1 public-key and updating source list.."
 	echo ""
 	wget -O- https://download.docker.com/linux/debian/gpg | apt-key add -
 	
         # SOURCE
         if [ -f /etc/apt/sources.list.d/docker.list  ] ; then sudo rm '/etc/apt/sources.list.d/docker.list'; fi
 	sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu/ $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list'
-        echo "[*] Installing $var1.. " 
+        echo "[*] Installing $1.. " 
 	echo ""
 	apt-get update -y
 	apt-get install docker-ce -y
--- lib/jenk.sh
+++ lib/jenk.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
 jenkins() {
-  echo "hello"
+    apt-get update -y
+    apt-get install openjdk-11-jdk -y
+    apt-get install net-tools -y
+    mkdir /usr/jenkins
+    cd /usr/jenkins
+    wget https://get.jenkins.io/war-stable/2.332.3/jenkins.war
+    java -jar jenkins.war --httpPort=8082 &
+    chck
   }
+
+chck() {
+    P=$(netstat -nutap | awk ' { if($4 == ":::8082") print $4 }') 
+    if [ "$P" = ":::8082" ] ; then
+        echo "[*] Jenkins is running."
+    else
+	echo "[*] Jenkins not active"
+    fi
+}
